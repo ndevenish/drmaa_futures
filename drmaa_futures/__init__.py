5f57b28a6766a48cf6ef0fce1fc82fadc69f8881
Nicholas Devenish <ndevenish@gmail.com>
2018-04-18 00:35:13 +0100

    Add an executor

--- drmaa_futures/__init__.py
+++ drmaa_futures/__init__.py
@@ -5,3 +5,5 @@
 __author__ = """Nicholas Devenish"""
 __email__ = 'ndevenish@gmail.com'
 __version__ = '0.1.0'
+
+from .executor import DRMAAPoolExecutor
---
+++ drmaa_futures/executor.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""Main module."""
+
+from concurrent.futures import Executor
+
+class DRMAAPoolExecutor(Executor):
+  """Control and submit to a pool of DRMAA-jobs."""
+  def __init__(self, max_jobs=None):
+    """Initialize a DRMAAPoolExecutor instance.
+
+    :param int max_jobs: The maximum number of jobs to launch.
+    """
+    self._max_jobs = max_jobs
+

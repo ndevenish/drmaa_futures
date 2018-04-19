8959c00386efcc22585911bc2d13cf65e28e0c95
Nicholas Devenish <ndevenish@gmail.com>
2018-04-19 14:32:36 +0100

    Generate a slave id if not provided with one

--- drmaa_futures/__main__.py
+++ drmaa_futures/__main__.py
@@ -10,11 +10,26 @@ Usage: python -mdrmaa_futures slave <server_and_port>
 from argparse import ArgumentParser
 import sys
 import os
+import uuid
 
 import logging
 # If __main__, we are the root logger
 logger = logging.getLogger()
 
+
+def generate_slave_name():
+  if "JOB_ID" in os.environ and "SGE_TASK_ID" in os.environ:
+    # We appear to be running inside an SGE-style system
+    task_id = os.environ["SGE_TASK_ID"]
+    if task_id == "undefined":
+      # SGE single job, no array.
+      return os.environ["JOB_ID"]
+    else:
+      # We're evidently in an array job!
+      return "{}.{}".format(os.environ["JOB_ID"], task_id)
+  else:
+    # Not sure what this is - use UUID4
+    return uuid.uuid4().hex
 
 def _run_main():
   # intention: drmaa_futures [-v|(-h | --help)] slave [(-h|--help)] <url> <id>
@@ -24,7 +39,7 @@ def _run_main():
   subparsers = parser.add_subparsers(help='Mode to run in', dest='command')
   parser_slave = subparsers.add_parser("slave", help="Run a slave node")
   parser_slave.add_argument("url", type=str)
-  parser_slave.add_argument("id", type=str)
+  parser_slave.add_argument("id", type=str, nargs="?")
   parser_slave.add_argument(
       '--timeout',
       action='store',
@@ -43,6 +58,10 @@ def _run_main():
   logging.basicConfig(
       level=logging.INFO if args.verbose == 0 else logging.DEBUG)
 
+  # Generate a slave name automatically if not provided
+  if args.id is None:
+    args.id = generate_slave_name()
+
   # Copy over the library_path
   library_path = os.environ.get("LD_LIBRARY_PATH", "").split(":")
   backup_path = os.environ.get("_LD_LIBRARY_PATH", "").split(":")
@@ -51,7 +70,7 @@ def _run_main():
     new_path = backup_path + [x for x in library_path if x not in backup_path]
     os.environ["LD_LIBRARY_PATH"] = ":".join(new_path)
 
-  logger.info("Starting slave node with master {}".format(args.url))
+  logger.info("Starting slave node {} with master {}".format(args.id, args.url))
   from drmaa_futures.slave import run_slave
   run_slave(args.url, args.id, timeout=args.timeout)
 
